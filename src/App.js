9779b8cb766071b4bf4b2e2c1c6c76e8c2bc6562
Henrique Viegas <dagostiniviegas@gmail.com>
2020-11-01 16:02:32 -0300

    riação do componente 'APP' com função de criação e delete

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `repositorio ${Math.random()}`,
+    })
+    const repository = response.data
+    setRepositories([...repositories, repository])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepositories(repositories.filter(repository => repository.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => <li key={repository.id}>{repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
+
+
         </li>
+        )}
       </ul>
+
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
@@ -29,3 +46,4 @@ function App() {
 }
 
 export default App;
+
